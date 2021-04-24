cfb67e40eeb7fe68aa18e0ca88dd9b30fe066b2e
ssl-27 <67239147+ssl-27@users.noreply.github.com>
2021-04-24 20:34:54 +0800

    Some new edits

--- main.cpp
+++ main.cpp
@@ -1,36 +1,45 @@
 #include <iostream>
 #include <cctype>
-#include "battle.h"
+
+using namespace std;
+
+//#include "battle.h"
 #include "randomMonster.h"
 #include "inventory.h"
 
-using namespace std;
+
 
 int main(){
     int choice;
 
     cout << "Save my Bonnie. Be the hero kid. " << endl;
+    cout << "Main menu: " << endl;
     cout << "1. Search for mobs." << endl;
     cout << "2. Fight the boss." << endl;
     cout << "3. Check inventory." << endl;
-    cout << "Your choice?";
+    cout << "Your choice? ";
     cin >> choice;
-    while (choice != 1 || choice != 2 || choice != 3 || choice != 0 ){
-        cout << "Wrong command!";
-        cin >> choice;
+    while (choice !=0 ){
+        if (choice == 1){
+            //battle(randomMonster());
+        }
+        else if (choice == 2 ){
+            //battle(100);
+        }
+        else if (choice == 3){
+            cout << "" << endl;
+            inventory();
+        }
+        else {
+            cout << "Wrong command!" << endl;
+        }
+        cout << "Main menu: " << endl;
+        cout << "1. Search for mobs." << endl;
+        cout << "2. Fight the boss." << endl;
+        cout << "3. Check inventory." << endl;
+        cout << "Your choice? ";        
+        cin >> choice;        
     }
-    if (choice == 1){
-        battle(randomMonster());
-    }
-    else if (choice == 2 ){
-        battle(100);
-    }
-    else if (choice == 3){
-        inventory();
-    }
-    main();
-    if (choice == 0){
-        return 0;
-    }
+
 
 }
--- randomMonster.h
+++ randomMonster.h
@@ -1,3 +1,5 @@
 int randomMonster(){
-    
+    int monsterCode;
+    monsterCode = rand()%3;
+    return monsterCode;
 }
