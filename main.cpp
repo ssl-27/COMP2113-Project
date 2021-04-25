48ad8131036eba4a78e5e21a6174a9a3fe240156
Fatshing <80964656+Fatshing@users.noreply.github.com>
2021-04-26 02:43:07 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(){
     getMounted();
     getUserInfo();
     compileUser();
-
+// start menu for the game
     cout << "Save my Bonnie. Be the hero kid. " << endl;
     cout << "Main menu: " << endl;
     cout << "1. Search for mobs." << endl;
@@ -25,14 +25,18 @@ int main(){
     cout << "0. Exit." << endl;
     cout << "Your choice? ";
     cin >> choice;
+    //use while loop to read the instruction, stops until "0" is inputed
     while (choice !=0 ){
         if (choice == 1){
+            //jump to the battle function which generate monster
             cout << battle(randomMonster(), withItems) << endl;
         }
         else if (choice == 2 ){
+            //jump to the battle with the boss
             cout << battle(100, withItems) << endl;
         }
         else if (choice == 3){
+            // jump to the display inventory function
             cout << "" << endl;
             inventory();
         }
@@ -43,6 +47,7 @@ int main(){
             printUserInfo(withItems);
         }
         else {
+            //verifcation for the input
             cout << "Wrong command!" << endl;
         }
         cout << "Main menu: " << endl;
