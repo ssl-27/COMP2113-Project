0b6795db10e6ea370ee37a052a63173c88a209c2
Fatshing <80964656+Fatshing@users.noreply.github.com>
2021-04-26 03:17:46 +0800

    Update randomMonster.h

--- randomMonster.h
+++ randomMonster.h
@@ -1,4 +1,5 @@
 int randomMonster(){
+    //this function returns a random value from 1 to 3 to represent the monster code
     int monsterCode;
     monsterCode = rand()%3 + 1;
     return monsterCode;
